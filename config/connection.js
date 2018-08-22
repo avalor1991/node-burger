f43947f10c59961e63ef4f24b8a83b31fba77671
Vitalii <avalor1991@gmail.com>
2018-08-21 23:23:02 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,33 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+var connection;
 
-connection.connect(function(err){
-    if (err) {
-        console.log("Error connecting: " + err.back);
-        return;
-    }
-    console.log("Connected as id: " + connection.threadId);
-});
+if (process.env.env.JAWDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "hacktheplanet",
+        database: "burgers_db"
+    })
+}
 
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
+
+// connection.connect(function(err){
+//     if (err) {
+//         console.log("Error connecting: " + err.back);
+//         return;
+//     }
+//     console.log("Connected as id: " + connection.threadId);
+// });
+
+connection.connect();
 module.exports = connection;
